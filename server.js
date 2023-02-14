2f89b73d89d9f5a1f849caf148e8109bca5a2e88
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-02-14 13:19:21 +0530

    cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://mern_quiz.onrender.com"],
     // credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
