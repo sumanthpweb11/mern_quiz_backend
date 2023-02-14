921c023c47e9c19ae2ea6b742b9fce7089eea517
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-02-14 16:45:05 +0530

    changes in server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://mern-quiz-app.onrender.com"],
+    origin: "*",
     // credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
