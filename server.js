176dc0fcac00fac426d187528699c2c3c8b03a76
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-02-14 14:56:14 +0530

    cors

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
