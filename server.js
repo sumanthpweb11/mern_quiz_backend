2196e5ab10b1b7521f439657a829534008dd99b3
sumanthpweb11 <sumanthpweb11@gmail.com>
2023-02-14 14:57:36 +0530

    cors

--- server.js
+++ server.js
@@ -33,10 +33,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Authorization, Accept"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
